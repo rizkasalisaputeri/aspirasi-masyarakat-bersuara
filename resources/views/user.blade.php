faa0619046c8ac345d38f85dea9b7a1261b5fab6
rizkasalisaputeri <112569887+rizkasalisaputeri@users.noreply.github.com>
2022-09-22 14:39:32 +0700

    update fitur laporan

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -72,12 +72,10 @@
                     <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
                     <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Input Laporan</a>
                         <div class="dropdown-menu bg-transparent border-0">
-                            <a href="signin.html" class="dropdown-item">Sign In</a>
-                            <a href="signup.html" class="dropdown-item">Sign Up</a>
-                            <a href="404.html" class="dropdown-item">404 Error</a>
-                            <a href="blank.html" class="dropdown-item">Blank Page</a>
+                            <a href="signin.html" class="dropdown-item">Laporan Proposal</a>
+                            <a href="signup.html" class="dropdown-item">Laporan Anggaran</a>
                         </div>
                     </div>
                 </div>
