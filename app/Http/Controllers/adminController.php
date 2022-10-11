38d88d34882968081af82b77706a40b7a7529af4
tasyarn <tasya.arinastia61@gmail.com>
2022-10-11 11:41:45 +0700

    edit controller user dan admin

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    public function showDashAdm()
+    {
+        return view('admin/dashboardAdm');
+    }
+
+    public function showEditKategori()
+    {
+        return view('admin/editKategori');
+    }
+    
+    public function showEditLaporan()
+    {
+        return view('admin/editLaporan');
+    }
+
+    public function showEditLokasi()    
+    {
+        return view('admin/editLokasi');
+    }
+
+    public function showEditUser()
+    {
+        return view('admin/editUser');
+    }
+}
